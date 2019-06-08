f7fda46ec50471a6e44bc8a865f99ef1d0b8345e
Eric Kim <bosung90@gmail.com>
2019-06-08 01:36:20 -0700

    If the app is unpinned, start the task again so it can be pinned

--- android/src/main/java/com/rnlocktask/MyAdmin.java
+++ android/src/main/java/com/rnlocktask/MyAdmin.java
@@ -4,5 +4,10 @@ import android.app.admin.DeviceAdminReceiver;
 
 
 public class MyAdmin extends DeviceAdminReceiver{
-
+  @Override
+  public void onLockTaskModeExiting(Context context, Intent intent) {
+    Intent activityIntent = new Intent(context, MainActivity.class);
+    activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    context.startActivity(activityIntent);
+  }
 }
